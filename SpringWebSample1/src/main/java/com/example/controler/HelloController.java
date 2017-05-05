6a22f48439551709bbdb4c35b5437552fb2ed156
Takuya Horimatsu <new_woory@hotmail.com>
2017-05-05 17:30:45 +0900

    MySQL接続サンプル

--- SpringWebSample1/src/main/java/com/example/controler/HelloController.java
+++ SpringWebSample1/src/main/java/com/example/controler/HelloController.java
@@ -1,13 +1,29 @@
 package com.example.controler;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
+import javax.persistence.Query;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
+	@PersistenceContext
+    private EntityManager entityManager;
+    
 	@RequestMapping("/world")
 	public String world() {
 		return "Hello world 2017";
+	}
+
+	@RequestMapping("/world2")
+	public String world2() {
+		Query query = entityManager.createNativeQuery("select 'name' from mydata LIMIT 1");
+		Object o = query.getSingleResult();
+		return "HW2 " + o.toString();
 	}
 }
