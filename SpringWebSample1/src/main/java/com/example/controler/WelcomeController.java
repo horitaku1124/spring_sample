df92c1d10643d95210cb78ca757495d0b6616c6c
Horimatsu Takuya <new_woory@hotmail.com>
2018-10-10 22:53:54 +0900

    file upload sample

--- SpringWebSample1/src/main/java/com/example/controler/WelcomeController.java
+++ SpringWebSample1/src/main/java/com/example/controler/WelcomeController.java
@@ -1,11 +1,16 @@
 package com.example.controler;
 
+import java.io.IOException;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import com.example.repositories.SampleTableRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class WelcomeController {
@@ -30,5 +35,26 @@ public class WelcomeController {
   public String welcome2(Map<String, Object> model) {
     model.put("all_count", "All: " + sampleTable.findAll().size());
     return "welcome2";
+  }
+
+  @RequestMapping(value = "/uploadForm", method = RequestMethod.GET)
+  public String uploadForm(Map<String, Object> model) {
+    return "uploadForm";
+  }
+  @RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
+  public String upload(Map<String, Object> model,
+                       @RequestParam("file") MultipartFile file,
+                       @RequestParam("comment") String comment,
+                       RedirectAttributes attr) throws IOException {
+    if (file.isEmpty()) {
+      attr.addFlashAttribute("message", "File is empty");
+      return "redirect:uploadForm";
+    }
+    byte[] data = file.getBytes();
+//    String body = new String(data, 0, data.length);
+    model.put("file_size", data.length);
+    model.put("comment", comment);
+    System.out.println("comment=" + comment);
+    return "uploadFile";
   }
 }
