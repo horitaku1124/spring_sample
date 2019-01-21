96a23629e009fd20d8903111955aefbcdd89634c
Horimatsu Takuya <new_woory@hotmail.com>
2019-01-21 20:47:35 +0900

    modify

--- docker/build_start.py
+++ docker/build_start.py
@@ -3,7 +3,7 @@ import os, sys
 
 
 os.chdir("../")
-print ("Current working dir : %s" % os.getcwd())
+print ("Mount Dir: %s" % os.getcwd())
 pwd = os.getcwd()
 
 client = docker.from_env()
@@ -21,64 +21,32 @@ container = client.containers.create('openjdk:8',
 container.start()  
 print('Container Started : {}'.format(container.status))
 
-print('1')
-log = container.exec_run('ln -s /mnt/spring_sample/_maven_home /root/.m2',
-                                  stdout=True,
-                                  stderr=True,
-                                  stream=True)
+workdir = "/mnt/spring_sample/SpringWebSample1"
 
-for line in log[1]:
-    print(line)
-
-
-print('2')
-log = container.exec_run('chmod +x mvnw',
-                                  workdir="/mnt/spring_sample/SpringWebSample1",
-                                  stdout=True,
-                                  stderr=True,
-                                  stream=True)
-
-for line in log[1]:
-    print(line)
-
-print('3')
-log = container.exec_run('chmod +x /mnt/spring_sample/docker/build.sh',
-                                  workdir="/mnt/spring_sample/SpringWebSample1",
-                                  stdout=True,
-                                  stderr=True,
-                                  stream=True)
-
-for line in log[1]:
-    print(line)
 
 env = {
     "JAVA_HOME": "/docker-java-home",
 }
 
+commands = [
+    'ln -s /mnt/spring_sample/_maven_home /root/.m2',
+    'chmod +x mvnw',
+    './mvnw -e install -Dmaven.test.skip=true',
+    './mvnw spring-boot:run',
+]
 
-print('7')
-log = container.exec_run('./mvnw -e install -Dmaven.test.skip=true',
-                                  workdir="/mnt/spring_sample/SpringWebSample1",
-                                  stdout=True,
-                                  stderr=True,
-                                  stream=True,
-                                  environment = env)
+for cmd in commands:
+    print('>' + cmd)
+    log = container.exec_run(cmd,
+                            workdir=workdir,
+                            stdout=True,
+                            stderr=True,
+                            stream=True,
+                            environment=env)
 
-for line in log[1]:
-    print(str(line))
+    for line in log[1]:
+        print(line)
 
-
-
-print('8')
-log = container.exec_run('./mvnw spring-boot:run',
-                                  workdir="/mnt/spring_sample/SpringWebSample1",
-                                  stdout=True,
-                                  stderr=True,
-                                  stream=True,
-                                  environment = env)
-
-for line in log[1]:
-    print(str(line))
 
 container.stop()
 print(container.status)
