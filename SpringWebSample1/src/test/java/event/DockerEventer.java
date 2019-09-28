d00c0e1bdfad718d5d92f86e85650b126339e1cc
Horimatsu Takuya <new_woory@hotmail.com>
2019-09-28 10:20:46 +0900

    fix docker eventer

--- SpringWebSample1/src/test/java/event/DockerEventer.java
+++ SpringWebSample1/src/test/java/event/DockerEventer.java
@@ -7,30 +7,23 @@ import com.spotify.docker.client.exceptions.DockerCertificateException;
 import com.spotify.docker.client.exceptions.DockerException;
 import com.spotify.docker.client.messages.ContainerConfig;
 import com.spotify.docker.client.messages.ContainerCreation;
-import com.spotify.docker.client.messages.HostConfig;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.regex.Pattern;
 
 /**
  * https://github.com/spotify/docker-client/blob/master/docs/user_manual.md
  */
 public class DockerEventer {
-    protected DockerClient docker;
-    protected ContainerCreation creation = null;
-    public DockerEventer(String dockerCommand) throws DockerCertificateException, DockerException, InterruptedException {
-        String[] bow = dockerCommand.split(" +");
-        ContainerConfig.Builder builder1 = ContainerConfig.builder();
-        switch (bow[0]) {
-            case "docker":
-                String operation = bow[1];
-//                String option = bow[2];
-                String image = bow[2];
-                builder1.image(image);
-//                String command = bow[4];
-                break;
-            default:
-                throw new RuntimeException("ng");
-        }
+    private final DockerClient docker;
+    protected ContainerCreation creation;
+    private List<Consumer<String>> stdoutReceivers = new ArrayList<>();
+    private Thread mainThread;
+    public DockerEventer(ContainerConfig.Builder builder1) throws DockerCertificateException, DockerException, InterruptedException {
+
 
 //        HostConfig hostConfig = HostConfig.builder()
 //                .appendBinds("/var/lib:/mnt/lib")
@@ -47,25 +40,96 @@ public class DockerEventer {
         DefaultDockerClient.Builder builder = DefaultDockerClient.fromEnv();
         docker = builder.build();
         creation = docker.createContainer(builder1.build());
-
-        docker.startContainer(creation.id());
-
-        final String logs;
-        try (LogStream stream = docker.logs(creation.id(), DockerClient.LogsParam.stdout(), DockerClient.LogsParam.stderr())) {
-//            logs = stream.readFully();
-            stream.forEachRemaining(s -> {
-                int len = s.content().remaining();
-                byte[] bytes = new byte[len];
-                s.content().get(bytes);
-                String str = new String(bytes, 0, len, StandardCharsets.UTF_8);
-                System.out.print("Buff:" + str);
-            });
+    }
+    public void startAndWaitForBoot(String pattern) throws DockerException, InterruptedException {
+        Pattern pattern1 = Pattern.compile(pattern);
+        final boolean[] started = {false};
+        this.stdoutReceivers.add(s -> {
+            if (pattern1.matcher(s).find()) {
+                System.out.println(" booted");
+                started[0] = true;
+            }
+        });
+        start();
+        int timeout = 100;
+        while(mainThread.isAlive()) {
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            if (started[0]) {
+                break;
+            }
+            timeout--;
+            if (timeout < 0) {
+                throw new RuntimeException("timeout to start");
+            }
         }
-//        System.out.println(logs);
+    }
+
+    public void start() throws DockerException, InterruptedException {
+        docker.startContainer(creation.id());
+        if (stdoutReceivers.isEmpty()) {
+            return;
+        }
+        mainThread = new Thread(() -> {
+            try (LogStream stream = docker.logs(creation.id(), DockerClient.LogsParam.stdout(), DockerClient.LogsParam.stderr())) {
+                stream.forEachRemaining(s -> {
+                    int len = s.content().remaining();
+                    byte[] bytes = new byte[len];
+                    s.content().get(bytes);
+                    String str = new String(bytes, 0, len, StandardCharsets.UTF_8);
+                    stdoutReceivers.forEach(consumer -> {
+                        consumer.accept(str);
+                    });
+                });
+            } catch (InterruptedException | DockerException e) {
+                e.printStackTrace();
+            }
+        });
+        mainThread.start();
     }
 
     public void removeWhenFinish() throws DockerException, InterruptedException {
         docker.stopContainer(creation.id(), 10);
         docker.removeContainer(creation.id());
+        docker.close();
+    }
+
+    public static class DockerEventerOption {
+        private ContainerConfig.Builder builder1;
+        private List<Consumer<String>> stdoutReceivers = new ArrayList<>();
+
+        public DockerEventerOption addStdoutReceiver(Consumer<String> receiver) {
+            this.stdoutReceivers.add(receiver);
+            return this;
+        }
+        public DockerEventer build() throws InterruptedException, DockerException, DockerCertificateException {
+            DockerEventer dockerEventer = new DockerEventer(builder1);
+            dockerEventer.stdoutReceivers.addAll(stdoutReceivers);
+            return dockerEventer;
+        }
+    }
+
+    public static class DockerEventerBuilder {
+        public static DockerEventerOption command(String dockerCommand) {
+            String[] bow = dockerCommand.split(" +");
+            ContainerConfig.Builder builder1 = ContainerConfig.builder();
+            switch (bow[0]) {
+                case "docker":
+                    String operation = bow[1];
+//                String option = bow[2];
+                    String image = bow[2];
+                    builder1.image(image);
+//                String command = bow[4];
+                    break;
+                default:
+                    throw new RuntimeException("ng");
+            }
+            DockerEventerOption option = new DockerEventerOption();
+            option.builder1 = builder1;
+            return option;
+        }
     }
 }
--- SpringWebSample1/src/test/java/event/DockerTest.java
+++ SpringWebSample1/src/test/java/event/DockerTest.java
@@ -2,12 +2,26 @@ package event;
 
 import com.spotify.docker.client.exceptions.DockerCertificateException;
 import com.spotify.docker.client.exceptions.DockerException;
+import static event.DockerEventer.DockerEventerOption;
+import static event.DockerEventer.DockerEventerBuilder.command;
 import org.junit.Test;
 
 public class DockerTest {
     @Test
     public void test1() throws InterruptedException, DockerException, DockerCertificateException {
-        DockerEventer dockerEventer = new DockerEventer("docker run hello-world");
-        dockerEventer.removeWhenFinish();
+        DockerEventer eventer = command("docker run hello-world")
+                .addStdoutReceiver(s -> System.out.print("S:" + s))
+                .build();
+        eventer.start();
+        Thread.sleep(1000);
+        eventer.removeWhenFinish();
+    }
+    @Test
+    public void test2() throws InterruptedException, DockerException, DockerCertificateException {
+        DockerEventer eventer = command("docker run hello-world")
+                .addStdoutReceiver(s -> System.out.print("S2:" + s))
+                .build();
+        eventer.startAndWaitForBoot("get-started");
+        eventer.removeWhenFinish();
     }
 }
